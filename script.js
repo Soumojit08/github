e7176fe43fd1abfc7d5045355e5736e2ff1dec71
Soumojit08 <soumojitbanerjee08@gmail.com>
2023-12-06 22:12:08 +0530

    finish of hero sec

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ const color = function () {
   });
 };
 color();
+
+const btnRight = document.querySelector(".btn-right");
+const btnLeft = document.querySelector(".btn-left");
+const img1 = document.querySelector("#img1");
+
+const images = [
+  "./Images/travel-flipkart.webp",
+  "./Images/Phone.webp",
+  "./Images/Zeb-comp.webp",
+];
+
+let currentImageindex = 0;
+
+btnRight.addEventListener("click", () => {
+  currentImageindex = (currentImageindex + 1)%images.length;
+  img1.setAttribute("src", images[currentImageindex]);
+});
+
+btnLeft.addEventListener("click", () => {
+  currentImageindex = (currentImageindex + images.length - 1)%images.length;
+  img1.setAttribute("src", images[currentImageindex]);
+});
